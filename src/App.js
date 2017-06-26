b9cc5f3296ec09c25d41037c3ba11760be80ad38
narghev <n.ghevandiani@gmail.com>
2017-06-26 14:56:07 -0400

    drag and drop working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import Draggable from 'react-draggable';
+import Fields from './components/fields';
 
 class App extends Component {
-  render() {
+    constructor(){
+	super();
+	this.state = {};
+    }
+
+    componentWillMount(){
+	const newItems = [[],[],[],[],[],[],[]];
+	for (let i = 0; i < newItems.length; i++){
+	    const l = Math.floor(Math.random()*6);
+	    for (let j = 0; j < l; j++){
+		const r = Math.floor(Math.random()*255);
+		const g = Math.floor(Math.random()*255);
+		const b = Math.floor(Math.random()*255);
+		const color = `rgb(${r},${g},${b})`;
+		newItems[i].push(color);
+	    }
+	}
+	this.setState({items: newItems});
+    }
+
+    changeColumn = (obj, newColumn) => {
+	const {x,y} = obj;
+	if (x === newColumn){
+	    return;
+	}
+	let itemsSnap = this.state.items;
+	const movedObj = itemsSnap[x][y];
+	itemsSnap[newColumn].push(movedObj);
+	delete itemsSnap[x][y];
+	itemsSnap = itemsSnap.filter((elem)=>{
+	    return elem !== undefined;
+	});
+	this.setState({items: itemsSnap});
+    }
+    render() {
     return (
 	<div className="App">
-	  <Draggable>
-            <div style={{backgroundColor: 'red', width: '200px', height: '400px'}}></div>
-	  </Draggable>
-      </div>
+	  <Fields items={this.state.items} changeColumn={this.changeColumn} />
+	</div>
     );
   }
 }
